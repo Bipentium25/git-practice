5f715e50ae2553c0fc031f7c85ea7657f6182838
Olga-allgood <olga.s.orlova@gmail.com>
2025-09-22 14:49:32 -0700

    function d Olga's

--- function_d.py
+++ function_d.py
@@ -1,9 +1,15 @@
+
+
 def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+    maximum_num = numbers[0]
+    for num in numbers:
+        if num > maximum_num:
+            maximum_num = num
+    return maximum_num
 
 
 if __name__ == "__main__":
-    print(max_value([1, 12, 2, 42, 8, 3]))
+    print(max_value([1, 12, 2, 42, 8, 3]))    
